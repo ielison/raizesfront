dd3e8accb5b281bcf03ab7c727114ba00bddb1e0
iélison dos santos barbosa <ielisonsbarbosa@gmail.com>
2024-09-11 11:04:25 -0300

    Update LoginModal.jsx

--- src/components/LoginModal/LoginModal.jsx
+++ src/components/LoginModal/LoginModal.jsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 export default function LoginModal({ isOpen, onClose, handleRegisterClick }) {
   const navigate = useNavigate();
   const { login } = useAuth(); // Using the login function from AuthContext
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
 
   const handleLoginSubmit = async (e) => {
     e.preventDefault();
@@ -46,33 +46,62 @@ export default function LoginModal({ isOpen, onClose, handleRegisterClick }) {
         </button>
         <h2>Olá, seja bem-vindo!</h2>
         <p>
-          Identificar e gerenciar pacientes de alto risco de câncer é crucial para oferecer cuidados personalizados e eficazes.
-          Com nossa ferramenta, você tem acesso rápido e seguro aos dados dos seus pacientes, permitindo que você tome decisões informadas e assertivas.
-          Agradecemos sua dedicação em cuidar da saúde não só dos seus pacientes, mas também das suas famílias!
+          Identificar e gerenciar pacientes de alto risco de câncer é crucial
+          para oferecer cuidados personalizados e eficazes. Com nossa
+          ferramenta, você tem acesso rápido e seguro aos dados dos seus
+          pacientes, permitindo que você tome decisões informadas e assertivas.
+          Agradecemos sua dedicação em cuidar da saúde não só dos seus
+          pacientes, mas também das suas famílias!
         </p>
         <p>
-          Realize login abaixo para iniciar um novo questionário de avaliação ou acessar as informações dos seus pacientes e continuar a fornecer o melhor cuidado possível:
+          Realize login abaixo para iniciar um novo questionário de avaliação ou
+          acessar as informações dos seus pacientes e continuar a fornecer o
+          melhor cuidado possível:
         </p>
-        
-        {errorMessage && <p className="login-modal-error-message">{errorMessage}</p>} {/* Display error message */}
-
+        {errorMessage && (
+          <p className="login-modal-error-message">{errorMessage}</p>
+        )}{" "}
+        {/* Display error message */}
         <form onSubmit={handleLoginSubmit}>
           <div className="login-modal-form-group">
             <label htmlFor="email">E-mail:</label>
-            <input type="email" id="email" name="email" placeholder="Informe o seu e-mail" required />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Informe o seu e-mail"
+              required
+            />
           </div>
           <div className="login-modal-form-group">
             <label htmlFor="password">Senha:</label>
-            <input type="password" id="password" name="password" placeholder="Digite a sua senha" required />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Digite a sua senha"
+              required
+            />
           </div>
-          <a href="#" className="login-modal-forgot-password">Esqueci a senha</a> {/* Link for forgot password */}
+          <a href="#" className="login-modal-forgot-password">
+            Esqueci a senha
+          </a>{" "}
+          {/* Link for forgot password */}
           <div className="login-modal-submit-button-container">
-            <button type="submit" className="login-modal-submit-button">Entrar</button>
+            <button type="submit" className="login-modal-submit-button">
+              Entrar
+            </button>
           </div>
         </form>
         <p className="login-modal-register-prompt">
           Ainda não possui uma conta?{" "}
-          <span className="login-modal-register-link" onClick={handleRegisterClick}>Registre-se aqui</span> {/* Register link */}
+          <span
+            className="login-modal-register-link"
+            onClick={handleRegisterClick}
+          >
+            Registre-se aqui
+          </span>{" "}
+          {/* Register link */}
         </p>
       </div>
     </div>
