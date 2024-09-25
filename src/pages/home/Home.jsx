f5a431e22aeb4cb7a27b9cc6b42198c8ee8952fa
iélison dos santos barbosa <ielisonsbarbosa@gmail.com>
2024-09-25 10:18:04 -0300

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,37 @@
+import React, { useEffect } from "react";
 import HomeHero from "../../components/HomeHero/HomeHero";
 import "./Home.css";
 
 export default function Home() {
+  useEffect(() => {
+    // Função para fazer o ping ao servidor
+    const pingServer = () => {
+      fetch("https://testserver-2p40.onrender.com/teste")
+        .then((response) => {
+          if (response.ok) {
+            console.log("Ping bem-sucedido ao servidor.");
+          } else {
+            console.error("Erro no ping ao servidor:", response.status);
+          }
+        })
+        .catch((error) => {
+          console.error("Erro ao fazer o ping:", error);
+        });
+    };
+
+    // Fazer o ping inicial
+    pingServer();
+
+    // Configurar o intervalo para fazer o ping a cada 2 minutos (120.000 ms)
+    const intervalId = setInterval(pingServer, 120000);
+
+    // Limpar o intervalo ao desmontar o componente
+    return () => clearInterval(intervalId);
+  }, []); // O array vazio garante que o efeito só será executado uma vez, após o componente ser montado
+
   return (
     <div className="homepage">
-    <HomeHero />
+      <HomeHero />
     </div>
   );
 }
