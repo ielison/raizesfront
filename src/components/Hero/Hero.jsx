c70aabf9ec71e7bb118a64b2a9e1b47fa5df9408
iélison dos santos barbosa <ielisonsbarbosa@gmail.com>
2024-09-25 10:35:46 -0300

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,41 @@
+import React, { useEffect } from "react";
 import "./Hero.css";
 import Instagram from "../../assets/instagram.svg";
 
 export default function Hero() {
+  useEffect(() => {
+    console.log("Hero component mounted"); // Verificar se o componente foi montado
+
+    // Função para fazer o ping ao servidor
+    const pingServer = () => {
+      console.log("Attempting to ping server..."); // Indicar que a função de ping foi chamada
+
+      fetch("https://testserver-2p40.onrender.com/teste")
+        .then((response) => {
+          if (response.ok) {
+            console.log("Ping bem-sucedido ao servidor.");
+          } else {
+            console.error("Erro no ping ao servidor:", response.status);
+          }
+        })
+        .catch((error) => {
+          console.error("Erro ao fazer o ping:", error);
+        });
+    };
+
+    // Fazer o ping inicial
+    pingServer();
+
+    // Configurar o intervalo para fazer o ping a cada 2 minutos (120.000 ms)
+    const intervalId = setInterval(pingServer, 120000);
+
+    // Limpar o intervalo ao desmontar o componente
+    return () => {
+      console.log("Cleaning up interval");
+      clearInterval(intervalId);
+    };
+  }, []); // O array vazio garante que o efeito só será executado uma vez, após o componente ser montado
+
   return (
     <div className="hero-container">
       <div className="hero-content">
@@ -24,7 +58,7 @@ export default function Hero() {
         <button className="hero-startButton">Começar avaliação</button>
       </div>
       <div>
-      <img className="instagram" src={Instagram} alt="instagram raízes" />
+        <img className="instagram" src={Instagram} alt="instagram raízes" />
       </div>
     </div>
   );
