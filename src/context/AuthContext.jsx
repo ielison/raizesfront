f04bc1f755c68715d302a173240ce54316560d13
iélison dos santos barbosa <ielisonsbarbosa@gmail.com>
2024-09-27 21:45:53 -0300

    Add files via upload

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useContext } from "react";
+import PropTypes from 'prop-types';
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [idUser, setIdUser] = useState(null);
+  const [nome, setNome] = useState(""); // Adiciona o estado para o nome
+
+  const login = (idUser, nome) => {
+    setIsLoggedIn(true);
+    setIdUser(idUser); // Armazena o idUser
+    setNome(nome); // Armazena o nome
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    setIdUser(null);
+    setNome(""); // Limpa o nome ao fazer logout
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, idUser, nome, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// Adiciona validação de PropTypes
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
---
+++ src/pages/LinksUteis2/LinksUteis2.jsx
@@ -0,0 +1,12 @@
+import Links from "../../components/Links/Links";
+import "./LinksUteis2.css";
+import Topbar from "../../components/HomeTopbar/HomeTopbar";
+
+export default function LinksUteis2() {
+  return (
+    <div className="links-uteis__items">
+      <Topbar />
+      <Links />
+    </div>
+  );
+}
