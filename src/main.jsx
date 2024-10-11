acf31b795a1d18ec548d4ba06f8662b0a9877075
iélison dos santos barbosa <ielisonsbarbosa@gmail.com>
2024-10-10 22:59:08 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ModalProvider } from "./context/ModalContext"; // Adjust path as needed
-import { AuthProvider } from "./context/AuthContext"; // Import AuthProvider
-import App from "./App";
+import ReactGA from "react-ga4";
+import Root from "./Root";
 
 import "./index.css";
 
+ReactGA.initialize("G-PG742HLHH4");
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <ModalProvider>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </ModalProvider>
-    </Router>
+    <Root />
   </StrictMode>
 );
