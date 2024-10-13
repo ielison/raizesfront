6c1520cdaca0deb36adb12a3acc929de4ebe75c1
iélison dos santos barbosa <ielisonsbarbosa@gmail.com>
2024-10-13 14:22:44 -0300

    Delete src directory

--- src/data/ageOptions.js
+++
@@ -1,6 +0,0 @@
-export const ageOptions = [
-  { value: "under_35", label: "Menor que 35 anos" },
-  { value: "35_50", label: "Entre 35 e 50 anos" },
-  { value: "over_50", label: "Maior que 50 anos" },
-  { value: "unknown", label: "Desconhecido" },
-];
